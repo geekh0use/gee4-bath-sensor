21a7f74bd2a19c98e0524e8656789fca7d6a99d1
mktoho12 <makoto_16n1200102@nnn.ed.jp>
2019-05-23 14:52:08 +0900

    インデントを修正

--- furo.rb
+++ furo.rb
@@ -4,8 +4,6 @@ require 'serialport'
 require 'httpclient'
 require 'daemon_spawn'
 
-#Process.daemon
-
 # ruby 2.6
 module Kernel
   def then(&block)
@@ -17,24 +15,24 @@ end
 class FuroDaemon < DaemonSpawn::Base
 
   def start(args)
-client = HTTPClient.new
-client.debug_dev = $stderr
-
-SerialPort.open '/dev/serial0' do |furo|
-  loop do
-    furo.readline.chomp
-      .then { |line| line =~ /^::rc=/ ? [line] : [] }
-      .map { |it|
-        values = it.split(/:/).map { |e| e.split(/=/) }
-        hash = Hash[*values.flatten]
-        hash[:ts] = Time.now.iso8601
-        JSON.generate(hash)
-      }
-      .each { |json| client.post_content('https://gee4-bath01.herokuapp.com/sensor_output', 
-                                         json, 'Content-Type' => 'application/json')
-      }
-  end
-end
+    client = HTTPClient.new
+    client.debug_dev = $stderr
+    
+    SerialPort.open '/dev/serial0' do |furo|
+      loop do
+        furo.readline.chomp
+          .then { |line| line =~ /^::rc=/ ? [line] : [] }
+          .map { |it|
+            values = it.split(/:/).map { |e| e.split(/=/) }
+            hash = Hash[*values.flatten]
+            hash[:ts] = Time.now.iso8601
+            JSON.generate(hash)
+          }
+          .each { |json| client.post_content('https://gee4-bath01.herokuapp.com/sensor_output', 
+                                             json, 'Content-Type' => 'application/json')
+          }
+      end
+    end
   end
 
   def stop
